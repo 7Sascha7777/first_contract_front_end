0316c8a1880cd5dbfe17fe178e7496b6e4007f69
prechetniak <Ssh7P1-6Y>
2024-07-14 18:44:14 +0200

    Initial commit

---
+++ src/hooks/useMainContract.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { MainContract } from "../contracts/MainContract";
+import { useTonClient } from "./useTonClient";
+import { useAsyncInitialize } from "./useAsyncInitialize";
+import { Address, OpenedContract } from "ton-core";
+import { toNano } from "ton-core";
+import { useTonConnect } from "./useTonConnect";
+
+export function useMainContract() {
+
+    // ... variables definitions
+    const { sender } = useTonConnect();
+    
+    const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const client = useTonClient();
+  const [contractData, setContractData] = useState<null | {
+    counter_value: number;
+    recent_sender: Address;
+    owner_address: Address;
+  }>();
+
+  const [balance, setBalacne] = useState<null | number>(0);
+
+  const mainContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new MainContract(
+      Address.parse("EQAbPpClxCwQSfZHedL0_vkXzu2KuDSdYOU50ho1MohPEqSe") // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<MainContract>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!mainContract) return;
+      setContractData(null);
+      const val = await mainContract.getData();
+      setContractData({
+        counter_value: val.number,
+        recent_sender: val.recent_sender,
+        owner_address: val.owner_address,
+      });
+      setBalacne(balance);
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [mainContract]);
+
+  return {
+
+    contract_address: mainContract?.address.toString(),
+    contract_balance: balance,
+    ...contractData,
+    sendIncrement: async () => {
+      return mainContract?.sendIncrement(sender, toNano(0.05), 3);
+    },
+  };
+}
